7ee774753a4529d05892b21a7b93ec4c98d8443f
khinhsumyatnoe <khinsumyatnoe771997@gmail.com>
2018-06-12 09:51:31 +0630

    Update my hello

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-return "Welcome to khinhsumyatnoe"
+    return "Hello World"
+
+@app.route("/tiide")
+def tiide():
+    return "Welcome to TIIDE World"
